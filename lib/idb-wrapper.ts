64ce662a1af267dbae6b237dabc2db22e3c28a4c
Eric Henderson <henderea@gmail.com>
2024-04-01 16:11:16 -0400

    split apart the idb code and enhance the type setup; i won't use IDB for now

---
+++ lib/idb-wrapper.ts
@@ -0,0 +1,102 @@
+import type { IDBPDatabase, IDBPTransaction, DBSchema, StoreNames, StoreKey, StoreValue } from 'idb';
+
+import * as idb from 'idb';
+
+import _each from 'lodash/each.js';
+import _slice from 'lodash/slice.js';
+
+export type UpgradeTransaction<DBTypes extends DBSchema | unknown = unknown> = IDBPTransaction<DBTypes, ArrayLike<StoreNames<DBTypes>>, 'versionchange'>;
+
+export type UpgradeFunc<DBTypes extends DBSchema | unknown = unknown> = (database: IDBPDatabase<DBTypes>, oldVersion: number, newVersion: number | null, transaction: UpgradeTransaction<DBTypes>) => void;
+
+export type DbUpgrade<DBTypes extends DBSchema | unknown = unknown> = { doUpgrade: UpgradeFunc<DBTypes>, logLines: any[] };
+
+export class DbUpgrades<DBTypes extends DBSchema | unknown = unknown> {
+  private readonly _upgrades: DbUpgrade<DBTypes>[] = [];
+
+  get upgrades(): DbUpgrade<DBTypes>[] { return this._upgrades; }
+
+  add(doUpgrade: UpgradeFunc<DBTypes>, ...logLines: any[]): this {
+    this.upgrades.push({ doUpgrade, logLines });
+    return this;
+  }
+
+  skipVersion(...logLines: any[]): this { return this.add(() => {}, ...logLines); }
+
+  upgrade(database: IDBPDatabase<DBTypes>, oldVersion: number, newVersion: number | null, transaction: UpgradeTransaction<DBTypes>): void {
+    _each(_slice(this.upgrades, oldVersion), (upgrade: DbUpgrade<DBTypes>) => {
+      _each(upgrade.logLines, console.log);
+      upgrade.doUpgrade(database, oldVersion, newVersion, transaction);
+    });
+  }
+
+  get length(): number { return this.upgrades.length; }
+}
+
+export class IndexedDB<DBTypes extends DBSchema | unknown = unknown> {
+  private readonly _db: Promise<IDBPDatabase<DBTypes>>;
+
+  constructor(db: Promise<IDBPDatabase<DBTypes>>) {
+    this._db = db;
+  }
+
+  store<StoreName extends StoreNames<DBTypes>>(name: StoreName): IndexedDBStore<DBTypes, StoreName> {
+    return new IndexedDBStore(this._db, name);
+  }
+}
+
+export class IndexedDBStore<DBTypes extends DBSchema | unknown = unknown, StoreName extends StoreNames<DBTypes> = StoreNames<DBTypes>> {
+  private readonly _db: Promise<IDBPDatabase<DBTypes>>;
+  private readonly _name: StoreName;
+
+  constructor(db: Promise<IDBPDatabase<DBTypes>>, name: StoreName) {
+    this._db = db;
+    this._name = name;
+  }
+
+  private get db(): Promise<IDBPDatabase<DBTypes>> { return this._db; }
+  private get tx(): Promise<IDBPTransaction<DBTypes, [StoreName], 'readwrite'>> {
+    return this.db.then((db: IDBPDatabase<DBTypes>) => db.transaction(this.name, 'readwrite'));
+  }
+  private get readTx(): Promise<IDBPTransaction<DBTypes, [StoreName], 'readonly'>> {
+    return this.db.then((db: IDBPDatabase<DBTypes>) => db.transaction(this.name));
+  }
+  get name(): StoreName { return this._name; }
+
+  async get(key: StoreKey<DBTypes, StoreName>): Promise<StoreValue<DBTypes, StoreName> | undefined> {
+    return (await this.readTx).objectStore(this.name).get(key);
+  }
+
+  async set(key: StoreKey<DBTypes, StoreName>, value: StoreValue<DBTypes, StoreName>): Promise<void> {
+    const tx: IDBPTransaction<DBTypes, [StoreName], 'readwrite'> = await this.tx;
+    await tx.objectStore(this.name).put(value, key);
+    return tx.done;
+  }
+
+  async delete(key: StoreKey<DBTypes, StoreName>): Promise<void> {
+    const tx: IDBPTransaction<DBTypes, [StoreName], 'readwrite'> = await this.tx;
+    await tx.objectStore(this.name).delete(key);
+    return tx.done;
+  }
+
+  async clear(): Promise<void> {
+    const tx: IDBPTransaction<DBTypes, [StoreName], 'readwrite'> = await this.tx;
+    await tx.objectStore(this.name).clear();
+    return tx.done;
+  }
+
+  async keys(): Promise<StoreKey<DBTypes, StoreName>[]> {
+    return (await this.readTx).objectStore(this.name).getAllKeys();
+  }
+}
+
+export function setupDb<DBTypes extends DBSchema | unknown = unknown>(dbName: string, dbUpgrades: DbUpgrades<DBTypes>): IndexedDB<DBTypes> {
+  const { upgrade } = dbUpgrades;
+  return new IndexedDB(idb.openDB(dbName, dbUpgrades.length, { upgrade }));
+}
+
+export {
+  IDBPDatabase,
+  IDBPTransaction,
+  DBSchema
+};
--- lib/idb.ts
+++ lib/idb.ts
@@ -1,93 +1,26 @@
-import * as idb from 'idb';
-import type { IDBPDatabase, IDBPTransaction, DBSchema, StoreNames } from 'idb';
+import type { IDBPDatabase, DBSchema, IndexedDB, IndexedDBStore } from './idb-wrapper';
 
-type UpgradeTransaction<DBTypes extends DBSchema | unknown = unknown> = IDBPTransaction<DBTypes, ArrayLike<StoreNames<DBTypes>>, 'versionchange'>;
+import { DbUpgrades, setupDb } from './idb-wrapper';
 
-type UpgradeFunc<DBTypes extends DBSchema | unknown = unknown> = (database: IDBPDatabase<DBTypes>, oldVersion: number, newVersion: number | null, transaction: UpgradeTransaction<DBTypes>) => void;
+export type ConfigKey = 'colors' | 'user-content';
 
-type DbUpgrade<DBTypes extends DBSchema | unknown = unknown> = { doUpgrade: UpgradeFunc<DBTypes>, logLines: any[] };
-
-class DbUpgrades<DBTypes extends DBSchema | unknown = unknown> {
-  private readonly _upgrades: DbUpgrade<DBTypes>[] = [];
-
-  get upgrades(): DbUpgrade<DBTypes>[] { return this._upgrades; }
-
-  add(doUpgrade: UpgradeFunc<DBTypes>, ...logLines: any[]): this {
-    this.upgrades.push({ doUpgrade, logLines });
-    return this;
+export interface Schema extends DBSchema {
+  config: {
+    key: ConfigKey,
+    value: string
   }
-
-  skipVersion(...logLines: any[]): this { return this.add(() => {}, ...logLines); }
-
-  upgrade(database: IDBPDatabase<DBTypes>, oldVersion: number, newVersion: number | null, transaction: UpgradeTransaction<DBTypes>): void {
-    this.upgrades.slice(oldVersion).forEach((upgrade: DbUpgrade<DBTypes>) => {
-      upgrade.logLines.forEach(console.log);
-      upgrade.doUpgrade(database, oldVersion, newVersion, transaction);
-    });
-  }
-
-  get length(): number { return this.upgrades.length; }
 }
 
-const dbUpgrades: DbUpgrades = new DbUpgrades<any>();
+const dbUpgrades: DbUpgrades<Schema> = new DbUpgrades<Schema>();
 
 // v1 - NOTE
-dbUpgrades.add((database: IDBPDatabase<any>, _oldVersion: number, _newVersion: number | null, _transaction: UpgradeTransaction<any>) => database.createObjectStore('config', { autoIncrement: true }));
+dbUpgrades.add((database: IDBPDatabase<Schema>) => database.createObjectStore('config', { autoIncrement: true }));
 
-export class IndexedDB {
-  private readonly _db: Promise<IDBPDatabase<any>>;
-
-  constructor(db: Promise<IDBPDatabase<any>>) {
-    this._db = db;
-  }
-
-  store(name: string): IndexedDBStore {
-    return new IndexedDBStore(this._db, name);
-  }
+export function openDb(): IndexedDB<Schema> {
+  return setupDb('hour-entry', dbUpgrades);
 }
 
-export class IndexedDBStore {
-  private readonly _db: Promise<IDBPDatabase<any>>;
-  public readonly name: string;
-
-  constructor(db: Promise<IDBPDatabase<any>>, name: string) {
-    this._db = db;
-    this.name = name;
-  }
-
-  async get(key: any): Promise<any> {
-    const db: IDBPDatabase<any> = await this._db;
-    return db.transaction(this.name).objectStore(this.name).get(key);
-  }
-
-  async set(key: any, value: any): Promise<void> {
-    const db: IDBPDatabase<any> = await this._db;
-    const tx: IDBPTransaction<any, [string], 'readwrite'> = db.transaction(this.name, 'readwrite');
-    await tx.objectStore(this.name).put(value, key);
-    return tx.done;
-  }
-
-  async delete(key: any): Promise<void> {
-    const db: IDBPDatabase<any> = await this._db;
-    const tx: IDBPTransaction<any, [string], 'readwrite'> = db.transaction(this.name, 'readwrite');
-    await tx.objectStore(this.name).delete(key);
-    return tx.done;
-  }
-
-  async clear(): Promise<void> {
-    const db: IDBPDatabase<any> = await this._db;
-    const tx: IDBPTransaction<any, [string], 'readwrite'> = db.transaction(this.name, 'readwrite');
-    await tx.objectStore(this.name).clear();
-    return tx.done;
-  }
-
-  async keys(): Promise<any[]> {
-    const db: IDBPDatabase<any> = await this._db;
-    return db.transaction(this.name).objectStore(this.name).getAllKeys();
-  }
-}
-
-export function openDb(): IndexedDB {
-  const { upgrade } = dbUpgrades;
-  return new IndexedDB(idb.openDB('hour-entry', dbUpgrades.length, { upgrade }));
-}
+export {
+  IndexedDB,
+  IndexedDBStore
+};
